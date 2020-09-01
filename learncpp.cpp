923066cfe06bba4beecc155ad54031f287a31f53
glof2 <karolwojcik4@gmail.com>
2020-09-01 19:48:33 +0200

    Fixed some memory issues

--- learncpp.cpp
+++ learncpp.cpp
@@ -6,7 +6,7 @@ int main()
 	String string{"Hi!\n"};
 	std::cout << "String length: " << string.length() << '\n';
 	std::cout << "String capacity: " << string.capacity_length() << '\n';
-	String str{"No"};
+	String str{"No!"};
 	string = str;
 	std::cout << "String length: " << string.length() << '\n';
 	std::cout << "String capacity: " << string.capacity_length() << '\n';
