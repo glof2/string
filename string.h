530ba34e17071638093887fccdbdc3bf6cd16caa
glof2 <karolwojcik4@gmail.com>
2020-09-01 13:48:36 +0200

    Added member functions and size_type

--- string.h
+++ string.h
@@ -3,10 +3,19 @@
 
 class String
 {
+public:
+	using size_type = unsigned long int;
 private:
-	;
+	char* m_start{ nullptr };
+	char* m_end{ nullptr };
+	char* m_capacity{ nullptr };
+	size_type size;
 public:
 	;
 };
 
 #endif
+
+// TODO:
+// 1. Private member functions - DONE
+// 2. 
